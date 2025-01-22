8d89c1b7fb31e782307709254a4ee8475f33693a
Minseong Jang <minseong.jang@kaist.ac.kr>
2025-01-22 15:49:11 +0900

    Update `extra_mesh` test

--- bareMetalC/extra_mesh.c
+++ bareMetalC/extra_mesh.c
@@ -23,7 +23,8 @@ int main() {
 
   printf("Initialize our input and output matrices in main memory\n");
   elem_t In[DIM][DIM];
-  elem_t Out[DIM][DIM];
+  elem_t Out0[DIM][DIM]; // Uses Mesh 0.
+  elem_t Out1[DIM][DIM]; // Uses Mesh 1.
 
   elem_t Identity[DIM][DIM];
   for (size_t i = 0; i < DIM; i++)
@@ -33,8 +34,9 @@ int main() {
   printf("Calculate the scratchpad addresses of all our matrices\n");
   printf("  Note: The scratchpad is \"row-addressed\", where each address contains one matrix row\n");
   size_t In_sp_addr = 0;
-  size_t Out_sp_addr = DIM;
-  size_t Identity_sp_addr = 2*DIM;
+  size_t Out_sp_addr0 = DIM;
+  size_t Out_sp_addr1 = 2*DIM;
+  size_t Identity_sp_addr = 3*DIM;
 
   printf("Move \"In\" matrix from main memory into Gemmini's scratchpad\n");
   gemmini_config_ld(DIM * sizeof(elem_t));
@@ -44,25 +46,45 @@ int main() {
   printf("Move \"Identity\" matrix from main memory into Gemmini's scratchpad\n");
   gemmini_mvin(Identity, Identity_sp_addr);
 
-  printf("Multiply \"In\" matrix with \"Identity\" matrix with a bias of 0\n");
+  printf("Multiply \"In\" matrix with \"Identity\" matrix with a bias of 0, using Mesh 0\n");
   gemmini_config_ex(OUTPUT_STATIONARY, 0, 0);
-  gemmini_preload_zeros2(Out_sp_addr);
+  gemmini_preload_zeros(Out_sp_addr0);
+  gemmini_compute_preloaded(In_sp_addr, Identity_sp_addr);
+
+  printf("Move \"Out0\" matrix from Gemmini's scratchpad into main memory\n");
+  gemmini_config_st(DIM * sizeof(elem_t));
+  gemmini_mvout(Out0, Out_sp_addr0);
+
+  printf("Multiply \"In\" matrix with \"Identity\" matrix with a bias of 0, using Mesh 1\n");
+  gemmini_preload_zeros2(Out_sp_addr1);
   gemmini_compute_preloaded2(In_sp_addr, Identity_sp_addr);
 
-  printf("Move \"Out\" matrix from Gemmini's scratchpad into main memory\n");
+  printf("Move \"Out1\" matrix from Gemmini's scratchpad into main memory\n");
   gemmini_config_st(DIM * sizeof(elem_t));
-  gemmini_mvout(Out, Out_sp_addr);
+  gemmini_mvout(Out1, Out_sp_addr1);
 
   printf("Fence till Gemmini completes all memory operations\n");
   gemmini_fence();
 
-  printf("Check whether \"In\" and \"Out\" matrices are identical\n");
-  if (!is_equal(In, Out)) {
+  printf("Check whether \"In\" and \"Out0\" matrices are identical\n");
+  if (!is_equal(In, Out0)) {
     printf("Input and output matrices are different!\n");
     printf("\"In\" matrix:\n");
     printMatrix(In);
-    printf("\"Out\" matrix:\n");
-    printMatrix(Out);
+    printf("\"Out0\" matrix:\n");
+    printMatrix(Out0);
+    printf("\n");
+
+    exit(1);
+  }
+
+  printf("Check whether \"In\" and \"Out1\" matrices are identical\n");
+  if (!is_equal(In, Out1)) {
+    printf("Input and output matrices are different!\n");
+    printf("\"In\" matrix:\n");
+    printMatrix(In);
+    printf("\"Out1\" matrix:\n");
+    printMatrix(Out1);
     printf("\n");
 
     exit(1);
